a2925e10c46d7feb278c06d161da1b970692b84e
Dong Son <dongson120204@gmail.com>
2023-10-06 09:59:36 +0700

    test1

--- 1.cpp
+++ 1.cpp
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <math.h>
+
+int check(int n){
+    for (int i=2; i<5;i++){break;}
+    return 0;
+}
 
 int main(){
-    printf("Hello !!");
+    int n, uoc=0;
+    printf("nhap n: ");
+    scanf("%d",&n);
+
+    for (int i=2; i <= sqrt(n);i++){
+        if (n%i==0) uoc++;
+    }
+    if (uoc==0) printf("%d la snt",n);
     return 0;
 }
